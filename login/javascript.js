2a1c3bb89cb354fb9f8750fde58dcb3ac1a7e607
Allison <91392629+allisonantunes@users.noreply.github.com>
2023-03-31 12:46:04 -0300

    js

---
+++ login/javascript.js
@@ -0,0 +1,12 @@
+document.getElementById("mes_ano").innerHTML = 'ola'
+let now = new Date
+
+/* document.write ("Hoje é " + now.getDay()
+ + ", " + now.getDate() + " de " + now.getMonth()
+  + " de " + now.getFullYear() ) */
+
+mes_nome = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
+
+/* mes_ano.innerHTML = mes_nome[now.getMonth()] */
+mes_ano = 'ola'
+
