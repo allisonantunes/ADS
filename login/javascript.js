295647e99e81faa1dd91458ef723a8efb74f21a6
Allison <91392629+allisonantunes@users.noreply.github.com>
2023-03-31 17:12:29 -0300

    js

--- login/javascript.js
+++ login/javascript.js
@@ -1,12 +1,11 @@
 let mes = document.getElementById("mes")
 let ano = document.getElementById("ano")
+let dias = document.getElementsByClassName("date-grid")
 let now = new Date
 
-/* document.write ("Hoje é " + now.getDay()
- + ", " + now.getDate() + " de " + now.getMonth()
-  + " de " + now.getFullYear() ) */
+document.write (now.getDate() )
 
 mes_nome = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
 
-mes.innerHTML = mes_nome[now.getMonth()]
-ano.innerHTML = ano[now. getFullYear()]
+mes.innerHTML = mes_nome[now.getMonth()].toUpperCase() + ","
+ano.innerHTML = now.getFullYear()
