cd8d277f722fdf051a411face5febb8c8561a94f
Allison <91392629+allisonantunes@users.noreply.github.com>
2023-04-18 20:51:30 -0300

    login

--- ZZZZZZZZZZZZZ/index.js
+++ ZZZZZZZZZZZZZ/index.js
@@ -2,28 +2,16 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const mysql = require('mysql2')
 const bodyParser = require('body-parser')
-
-const session = require('express-session')
-const flash = require('connect-flash')
+const cookieParser = require("cookie-parser")
+const sessions = require('express-session')
 
 const app = express()
 // pegar o body em json
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-)
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-//
 
 // sessao
-app.use(session({secret:"qualquercoisa"}))
-/* app.use(flash()) */
-//
-// middleware
-/* app.use((req, res, next) => {
-    res.locals
-}) */
+const oneDay = 1000 * 60 * 60 * 24
 
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
@@ -37,6 +25,17 @@ const conn = mysql.createConnection({
     database: 'agendamento_horarios',
 })
 
+
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false 
+}))
+
+app.use(cookieParser())
+var session
+
 conn.connect(function(err) {
     if(err) {
         console.log(err)
@@ -44,20 +43,26 @@ conn.connect(function(err) {
     console.log("conectou ao MySQL")
     app.listen(3000)
 })
-
-
 ////////////////////////////////////////////  fim  conexao com bd
 
 ////////////////////////////////////////////  login
 app.get('/', (req, res) => {
-    res.render("login")
+    session=req.session
+    if(session.userid){
+        res.render("agendarHorario")
+    }else{
+        res.render("login")
+    }
+    /* res.render("login") */
 })
+
+const myusername = 'asd'
+const mypassword = 'asdf'
 
 app.post('/', (req, res) => {
 
-    const username = req.body.user_name
+/*     const username = req.body.user_name
     const password = req.body.password
-    
     const sql = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
 
     conn.query(sql, function(err) {
@@ -65,12 +70,31 @@ app.post('/', (req, res) => {
             console.log(err)
             return
         }
-        res.redirect('/logado')
-    })
-    
+       res.redirect('/agendar')
+    }) */
+    if(req.body.user_name == myusername && req.body.password == mypassword){
+        session=req.session
+        session.userid=req.body.username
+        console.log(req.session)
+        res.render("agendarHorario")
+    }
+    else{
+        res.send('Invalid username or password');
+    }
+     
 })
-app.get('/logado', (req, res) => {
-    res.render("logado")
+
+app.get('/logout',(req,res) => {
+    req.session.destroy();
+    res.redirect('/');
+})
+
+app.get('/agendar', (req, res) => {
+    res.render("agendarHorario")
+})
+
+app.post('/agendar', (req, res) => {
+    res.render("agendarHorario")
 })
 
 //////////////////////////////////////////// fim  login
@@ -97,63 +121,3 @@ app.post('/cadastro', (req, res) => {
     })
 })
 ////////////////////////////////////////////  fim  cadastro
-
-
-/* const username = 'ttt'
-const password = 123
-
-app.post('/', (req, res) => {
-
-    if(req.body.usuario == username && req.body.senha == password){
-        req.session.username = username
-
-        res.render("logado")
-       
-    }else{
-         res.render("login")
-    }
-    
-})
-
-// pg inicial
-app.get('/', (req, res) =>{
-    if(req.session.login){
-        res.render('pagina logado')
-    }else{
-        res.render('login')
-    }
-    
-    res.redirect('/paginainicial')
-}) */
-
-/*
-app.get('/clientes', (req, res) => {
-    const slq = "SELECT * FROM users"
-
-    conn.query(slq, function (err, data) {
-        if(err) {
-            console.log(err)
-            return
-        }
-
-        const clientes = data
-        console.log(clientes);
-
-        res.render('clientes', {clientes})
-    })
-})
-
-app.get('/login/id', (req, res) => {
-
-    const id = req.params.id 
-
-    conn.query(sql, function(err, data) {
-        if(err) {
-            console.log(err)
-            return
-        }
-        const usuario = data[0]
-        res.render('login', {usuario})
-    })
-}) */
-
---
+++ ZZZZZZZZZZZZZ/views/layouts/javascript.js
@@ -0,0 +1,41 @@
+let mes = document.getElementById("mes")
+let ano = document.getElementById("ano")
+let dias = document.querySelector(".date-grid")
+let marcacao = document.querySelector(".confirmar_horario")
+
+let now = new Date
+ano_atual = now.getFullYear()
+mes_atual = now.getMonth()
+
+
+mes_nome = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
+
+mes.innerHTML = mes_nome[mes_atual].toUpperCase() + ","
+ano.innerHTML = ano_atual
+
+var meses = mes = mes_nome[mes_atual].toUpperCase()
+
+    let dias_mes = new Date(ano_atual, mes_atual + 1, 0).getDate()
+    let dias_tag = ""
+
+    for (let i = 1; i <= dias_mes; i++) {
+    dias_tag += `<button value="${i}" onclick='marca_dia(${i})'>${i}</button>`;
+    }
+    
+    dias.innerHTML = dias_tag
+    let dia_selecionado = ''
+    let hora_selecionada = ''
+
+    function marca_dia(dia){
+        dia_selecionado = dia
+        teste = `<div>Vc selecionou: ${dia_selecionado} de ${meses} de ${ano_atual} às ${hora_selecionada}</button>`;
+        marcacao.innerHTML = teste
+    };
+
+    function horarios(hora) {
+        hora_selecionada = hora
+
+        teste = `<div>Vc selecionou: ${dia_selecionado} de ${meses} de ${ano_atual} às ${hora}</button>`;
+        marcacao.innerHTML = teste
+        
+    };
